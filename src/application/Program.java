dfcb16eb1184856fb6deac7cf48213b93c1aae30
Rodrigo Oliveira <rodrigooliveira@MacBook-Air-de-Rodrigo.local>
2021-08-31 19:35:54 -0300

    Initial commit

---
+++ src/application/Program.java
@@ -0,0 +1,55 @@
+package application;
+
+import entities.LegalPerson;
+import entities.Person;
+import entities.PhysicalPerson;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Person> list = new ArrayList<>();
+
+        System.out.print("Enter the number of tax payers: ");
+        int n = sc.nextInt();
+
+        for (int i=1; i<=n; i++){
+            System.out.println("Tax payer #" + i + " data:");
+            System.out.print("Individual or company (i/c)? ");
+            char ch = sc.next().charAt(0);
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Anual income: ");
+            double anualIcome = sc.nextDouble();
+
+            if(ch == 'i'){
+                System.out.print("Health expenditures: ");
+                double healthExpenditures = sc.nextDouble();
+                list.add(new PhysicalPerson(name, anualIcome,healthExpenditures ));
+            }
+            else {
+                System.out.print("Number of employees: ");
+                int employeeNumber = sc.nextInt();
+                list.add(new LegalPerson(name, anualIcome, employeeNumber));
+            }
+        }
+        double sum = 0.0;
+        System.out.println();
+        System.out.println("TAXES PAID:");
+        for(Person p: list){
+            double tax = p.tax();
+            System.out.println(p.getName() + ": $" + String.format("%.2f", tax));
+            sum += tax;
+        }
+        System.out.println();
+        System.out.println("TOTAL TAXES: $ " + String.format("%.2f", sum));
+        sc.close();
+    }
+}
---
+++ src/entities/LegalPerson.java
@@ -0,0 +1,33 @@
+package entities;
+
+public class LegalPerson extends Person{
+
+    private Integer employeeNumber;
+
+    public LegalPerson(){
+
+    }
+
+    public LegalPerson(String name, Double annualIncome, Integer employeeNumber) {
+        super(name, annualIncome);
+        this.employeeNumber = employeeNumber;
+    }
+
+    public Integer getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(Integer employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    @Override
+    public double tax() {
+      if (employeeNumber > 10){
+          return getAnnualIncome() * 0.14;
+      }
+      else {
+          return getAnnualIncome() * 0.16;
+      }
+    }
+}
